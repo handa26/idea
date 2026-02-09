f11a05555a18bae1d19ef9bb4d16de16fd1b4544
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-02-09 10:10:55 +0800

    feat: setup main layout & form layout

---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,14 @@
+<nav class="border-b border-border px-6">
+    <div class="max-w-7xl mx-auto h-16 flex items-center justify-between">
+        <div>
+            <a href="/">
+              <img src="/images/logo.svg" alt="Idea Logo" width="100">
+            </a>
+        </div>
+
+        <div class="flex gap-x-5 items-center">
+            <a href="/login">Login</a>
+            <a href="/register" class="btn">Register</a>
+        </div>
+    </div>
+</nav>
