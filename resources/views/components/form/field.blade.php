b2e2f9417e43a436046ab2bfdcb4c308814c9f46
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-02-09 10:25:46 +0800

    feat: show an error message for input validation

--- resources/views/components/form/field.blade.php
+++ resources/views/components/form/field.blade.php
@@ -3,4 +3,8 @@
 <div class="space-y-2">
     <label for="{{ $name }}" class="label">{{ $label }}</label>
     <input type="{{ $type }}" class="input" id="{{ $name }}" name="{{ $name }}" {{ $attributes }}>
+
+    @error($name)
+        <p class="error">{{ $message }}</p>
+    @enderror
 </div>
