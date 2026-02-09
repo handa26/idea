a566363861b133a0855e46ff9203662da10156a4
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-02-09 10:44:42 +0800

    feat: conditionally display auth buttons, persist input value when fail to validate

--- resources/views/components/layout/nav.blade.php
+++ resources/views/components/layout/nav.blade.php
@@ -7,8 +7,18 @@
         </div>
 
         <div class="flex gap-x-5 items-center">
-            <a href="/login">Login</a>
-            <a href="/register" class="btn">Register</a>
+            @auth
+                <form action="/logout" method="POST">
+                    @csrf
+
+                    <button class="btn bg-red-400">Log Out</button>
+                </form>
+            @endauth
+
+            @guest    
+                <a href="/login">Login</a>
+                <a href="/register" class="btn">Register</a>
+            @endguest
         </div>
     </div>
 </nav>
