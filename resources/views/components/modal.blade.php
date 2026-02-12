5a544cc29f2146e31ac7216992764c90b6ffe3cf
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-02-12 16:58:54 +0800

    feat: create modal layout, add input form for idea creation, implement modal functionality

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -2,17 +2,22 @@
 
 <div x-data="{ show: false, name: @js($name) }" x-show="show" @open-modal.window="if($event.detail === name) show = true;" role="dialog"
     aria-modal="true" aria-labelledby="modal-{{ $name }}-title" :aria-hidden="!show" tabindex="-1"
+    @close-modal="show = false"
     class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-xs"
     @keydown.escape.windows = "show = false" x-transition:enter="ease-out duration-100"
     x-transition:enter-start="opacity-0 -translate-y-4 -translate-x-4" x-transition:enter-end="opacity-100"
     x-transition:leave="ease-in duration-100" x-transition:leave-start="opacity-100"
     x-transition:leave-end="opacity-0 -translate-y-4 -translate-x-4" style="display: none;">
-    <x-card @click.away="show = false">
-        <div>
+    <x-card @click.away="show = false" class="shadow-xl max-w-2xl w-full max-h-[80dvh] overflow-auto">
+        <div class="flex justify-between items-center">
             <h2 id="modal-{{ $name }}-title" class="text-xl font-bold">{{ $title }}</h2>
+
+            <button @click="show = false" aria-label="Close modal">
+                <x-icons.close />
+            </button>
         </div>
 
-        <div>
+        <div class="my-4">
             {{ $slot }}
         </div>
     </x-card>
