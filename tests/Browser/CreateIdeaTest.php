04eb7541ad32a8659ec06c6301fa4b70f7129a6e
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-02-17 09:55:36 +0800

    feat: add new test input for steps

--- tests/Browser/CreateIdeaTest.php
+++ tests/Browser/CreateIdeaTest.php
@@ -15,13 +15,19 @@ it('creates a new idea', function () {
     ->click('@submit-new-link-button')
     ->fill('@new-link', 'https://laracasts.com')
     ->click('@submit-new-link-button')
+    ->fill('@new-step', 'Do a thing')
+    ->click('@submit-new-step-button')
+    ->fill('@new-step', 'Do another thing')
+    ->click('@submit-new-step-button')
     ->click('Create')
     ->assertPathIs('/ideas');
 
-  expect($user->ideas()->first())->toMatchArray([
+  expect($idea = $user->ideas()->first())->toMatchArray([
     'title' => 'Some example title',
     'status' => 'completed',
     'description' => 'An example description',
     'links' => ['https://laracasts.com/series/laravel-from-scratch-2026', 'https://laracasts.com']
   ]);
+
+  expect($idea->steps)->toHaveCount(2);
 });
