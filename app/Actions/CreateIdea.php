fe92318a9d42533345bdb5bb367f28875094172b
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-02-19 12:21:32 +0800

    refactor: shorten unnecessary code

--- app/Actions/CreateIdea.php
+++ app/Actions/CreateIdea.php
@@ -31,11 +31,9 @@ class CreateIdea
         DB::transaction(function () use ($data, $attributes) {
             $idea = $this->user->ideas()->create($data);
 
-            $steps = collect($attributes['steps'] ?? [])->map(fn ($step) => ['description' => $step]);
+            // $steps = collect($attributes['steps'] ?? [])->map(fn ($step) => ['description' => $step]);
 
-            $idea->steps()->createMany(
-                $steps
-            );
+            $idea->steps()->createMany($attributes['steps'] ?? []);
         });
     }
 }
